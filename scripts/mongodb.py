1af6721e659678538fb6ed903cfa442d693570c2
zeerorg(Rishabh Gupta) <r.g.gupta@outlook.com>
2017-12-21 03:35:59 -0800

    Made mongodb install script

--- scripts/mongodb.py
+++ scripts/mongodb.py
@@ -1,2 +1,27 @@
+import run_wrapper
+
 def test():
     print("working ...")
+
+def install():
+    """
+    Installs MongoDB in ubuntu 16.04
+    """
+    commands = """
+        sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+        echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+        sudo apt-get update
+        sudo apt-get install mongodb-org -y
+        sudo systemctl start mongod
+        sudo systemctl enable mongod
+    """
+    print("Installing MongoDB 3.4 in ubuntu 16.04")
+    for command in commands.split("\n"):
+        script_output = run_wrapper.run_command(commands)
+        if not script_output.status:
+            print("Failed ...\n Error is: ")
+            print(script_output.output)
+            return
+        else:
+            print("Installation complete")
+    pass
---
+++ scripts/run_wrapper.py
@@ -0,0 +1,29 @@
+import subprocess
+import os
+import shlex
+
+
+class ScriptOutput:
+    def __init__(self, status, process, output=""):
+        self.status = status
+        self.process = process
+        self.output = output
+
+
+def run_command(commands, **kwargs):
+    commands = shlex.split(commands)
+    file = open("test_file", "w")
+    try:
+        process = subprocess.run(commands, stdout=file)
+        file.close()
+        file = open("test_file")
+        output = file.read()
+        file.close()
+        return ScriptOutput(True, process, output)
+    except Exception:
+        file.close()
+        file = open("test_file")
+        error = file.read()
+        file.close()
+        return ScriptOutput(False, None, error)
+    pass
