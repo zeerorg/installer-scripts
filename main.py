412699ae3d566e460aea29e699ed881088ce4540
zeerorg(Rishabh Gupta) <r.g.gupta@outlook.com>
2017-12-23 22:48:08 -0800

    Moved from arguments to commands

--- main.py
+++ main.py
@@ -7,16 +7,35 @@ def cli():
     pass
 
 
-@cli.command(name="mongodb")
-@click.argument("task", default="test")
-def task_mongo(task):
+@cli.group(name="mongodb")
+def mongo():
     """
     Admin tasks related to Mongo DB
     """
     click.echo("Working on MongoDB for Ubuntu 16.04")
-    if task == "install":
-        mongodb.install()
-    elif task == "test":
-        mongodb.test()
-    elif task == "uninstall":
-        mongodb.uninstall()
+
+
+@mongo.command(name="install")
+def mongo_install():
+    """
+    Install MongoDB
+    """
+    click.echo("\n")
+    mongodb.install()
+    click.echo("\n")
+
+
+@mongo.command(name="test")
+def mongo_test():
+    """
+    Test MongoDB
+    """
+    mongodb.test()
+
+
+@mongo.command(name="uninstall")
+def mongo_uninstall():
+    """
+    Uninstall MongoDB
+    """
+    mongodb.uninstall()
